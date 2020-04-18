9a25e3edea405f9cee4e82c76b88ccdfe9fc1525
AjinkyaTaranekar <ajinkyataranekar@gmail.com>
2020-04-18 20:31:33 +0530

    Cornout: Social Distancing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
 import 'package:gocorona/Screens/Around.dart';
 import 'package:gocorona/Screens/Home.dart';
 import 'package:gocorona/Screens/World.dart';
@@ -8,6 +11,10 @@ import 'package:gocorona/Screens/PreventionPage.dart';
 import 'package:gocorona/Screens/Help.dart';
 import 'package:gocorona/Welcome.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/services.dart'; 
+import 'dart:io';
+
+import 'package:vibration/vibration.dart'; 
 
 void main() => runApp(Gocorona());
 
@@ -35,12 +42,82 @@ class Gocorona extends StatelessWidget {
 
 class MyStatefulWidget extends StatefulWidget {
   MyStatefulWidget({Key key}) : super(key: key);
-
+  
   @override
   _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
 }
 
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  final FlutterBlue flutterBlue = FlutterBlue.instance;
+
+  scanForDevices(){
+    print("Scan");
+    flutterBlue.startScan();
+    
+    print("Started Scan");
+    var subscriptions = flutterBlue.scanResults.listen((results) {
+      
+        print("Results: -");
+        for (ScanResult r in results) {
+            print('${r.device.id} found! rssi: ${r.rssi} ');
+            alertUsingVibrations(r.rssi,-69);
+        }
+    });
+    print(subscriptions);
+        
+  }
+  alertUsingVibrations(int rssi, int txPower){
+    //social distancing
+    double distance = pow(10, ((txPower-rssi)/(10*2)));
+    print(distance);
+    if (distance <= 1.8){ 
+      print("vibrating");
+      Vibration.vibrate(pattern: [100, 2000, 100, 2000], intensities: [128, 150]);
+      AlertDialog(
+        title: Text("Social Distancing Alert"),
+        content: Text("Pls Maintain a distance of 6ft."),
+        backgroundColor: Colors.amberAccent,
+        actions: [
+          FlatButton(
+            child: Text("OK"),
+            onPressed: () { 
+              Vibration.cancel();
+            },
+          ),
+        ],
+      );
+    }
+    else{
+      Vibration.cancel();
+    }
+  }
+  ///Initialisation and listening to device state
+  void initState() {
+      super.initState();
+      print("here");
+      flutterBlue.state.listen((state) {
+      if (state == BluetoothState.off) {
+        //Alert user to turn on bluetooth.
+        AlertDialog(
+          title: Text("Turn on Bluetooth"),
+          content: Text("Turn on bluetooth for social distancing alert."),
+          backgroundColor: Colors.lightBlue,
+          actions: [
+            FlatButton(
+              child: Text("OK"),
+              onPressed: () { 
+                Vibration.cancel();
+              },
+            ),
+          ],
+        );
+      }
+      else if (state == BluetoothState.on) {
+        print("Bluetooth On");
+        scanForDevices(); 
+      }
+    });
+  }
   int _selectedIndex = 2;
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black);
